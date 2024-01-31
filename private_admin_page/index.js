2f42d80818a5335b7edad9f27684d58c8e66b428
dlanaras <70207362+dlanaras@users.noreply.github.com>
2024-01-31 21:26:41 +0100

    fix: remove uneccessary hacks

--- private_admin_page/index.js
+++ private_admin_page/index.js
@@ -4,36 +4,13 @@ const port = 3000;
 const hostname = '127.0.0.1';
 const fs = require('fs');
 const bodyParser = require('body-parser');
-/*const cors = function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-};*/
 
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(cors);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/passwords.html');
 });
-
-app.get('/static/index.html', (req, res) => {
-    res.sendFile(__dirname + "/static/index.html");
-})
-
-app.get('/static/styles.css', (req, res) => {
-    res.sendFile(__dirname + "/static/styles.css");
-})
-
-app.get('/static/script.js', (req, res) => {
-    res.sendFile(__dirname + "/static/script.js");
-})
-
-app.get('/static/passwords.json', (req, res) => {
-    res.sendFile(__dirname + "/static/passwords.json");
-})
 
 app.post('/', (req, res) => {
     let passwords = require(__dirname + '/static/passwords.json');
